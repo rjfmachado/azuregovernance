3834815c04889a8ac581c9fcfc16c968ace4e491
Ricardo Machado <rjfmachado@users.noreply.github.com>
2019-08-29 00:05:09 +0200

    test rbac assignments (#23)
    * test rbac assignments
    
    * fix resource group reference
    
    * test azurerm aliases
    
    * Azurerm alias configuration
    
    * clean comments
    
    * add more rbac assignments ro subsccription and rg
    
    * fix assignment names
    
    * disable role assignments
    
    * enable rbac assignments

--- src/terraform/sProject01.tf
+++ src/terraform/sProject01.tf
@@ -1,3 +1,11 @@
+provider "azurerm" {
+  alias           = "Project01"
+  subscription_id = var.sProject01id[var.deploymentStage]
+  client_id       = var.client_id
+  tenant_id       = var.tenant_id
+  client_secret   = var.client_secret
+}
+
 variable "sProject01id" {
   description = "The subscription id for Project 01 Subscription"
   default     = null
@@ -15,27 +23,37 @@ resource "azuread_group" "gProject01VMOperators" {
   name = "Project 01 VMOperators"
 }
 
-# resource "random_uuid" "uuidProject01CustomSupportAssignment" {}
+resource "azuread_group" "gProject01Auditors" {
+  name = "Project 01 Auditors"
+}
 
-# resource "azurerm_role_assignment" "assProject01CustomSupportAssignment" {
-#   name               = random_uuid.uuidProject01CustomSupportAssignment.result
-#   scope              = data.azurerm_subscription.sProject01.id
-#   role_definition_id = azurerm_role_definition.roleCustomSupport.id
-#   principal_id       = azuread_group.gProject01Support.id
-# }
+# Assignment of a Role scoped to a Management Group
+resource "azurerm_role_assignment" "assProject01CustomSupport" {
+  provider           = "azurerm.Project01"
+  scope              = data.azurerm_subscription.sProject01.id
+  role_definition_id = azurerm_role_definition.roleCustomSupport.id
+  principal_id       = azuread_group.gProject01Support.id
+}
 
-# resource "random_uuid" "uuidProject01VMRestarterAssignment" {}
+# Assignment of a Role scoped to the subscription
+resource "azurerm_role_assignment" "assProject01VMRestarter" {
+  provider           = "azurerm.Project01"
+  scope              = data.azurerm_subscription.sProject01.id
+  role_definition_id = azurerm_role_definition.roleVMRestarter.id
+  principal_id       = azuread_group.gProject01VMOperators.id
+}
 
-# resource "azurerm_role_assignment" "assProject01VMRestarterAssignment" {
-#   name               = random_uuid.uuidProject01VMRestarterAssignment.result
-#   scope              = data.azurerm_subscription.sProject01.id
-#   role_definition_id = azurerm_role_definition.roleVMRestarter.id
-#   principal_id       = azuread_group.gProject01VMOperators.id
-# }
+resource "azurerm_role_assignment" "assProject01Auditors" {
+  provider             = "azurerm.Project01"
+  scope                = data.azurerm_subscription.sProject01.id
+  role_definition_name = "Reader"
+  principal_id         = azuread_group.gProject01Auditors.id
+}
 
+# Role Definition scoped to subscription
 resource "azurerm_role_definition" "roleVMRestarter" {
-  name = "VM Restart"
-  //Store in the Management Group Root
+  provider    = "azurerm.Project01"
+  name        = "VM Restart"
   scope       = data.azurerm_subscription.sProject01.id
   description = "Members can restart Virtual Machines."
 
@@ -53,6 +71,33 @@ resource "azurerm_role_definition" "roleVMRestarter" {
   assignable_scopes = [
     data.azurerm_subscription.sProject01.id
   ]
+}
+
+resource "azurerm_resource_group" "rgTestRBACAssignment" {
+  provider = "azurerm.Project01"
+  name     = "testrbacassignment"
+  location = "West Europe"
+}
+
+resource "azurerm_role_assignment" "assTestCustomSupport" {
+  provider           = "azurerm.Project01"
+  scope              = azurerm_resource_group.rgTestRBACAssignment.id
+  role_definition_id = azurerm_role_definition.roleCustomSupport.id
+  principal_id       = azuread_group.gProject01Support.id
+}
+
+resource "azurerm_role_assignment" "assTestVMRestarter" {
+  provider           = "azurerm.Project01"
+  scope              = azurerm_resource_group.rgTestRBACAssignment.id
+  role_definition_id = azurerm_role_definition.roleVMRestarter.id
+  principal_id       = azuread_group.gProject01VMOperators.id
+}
+
+resource "azurerm_role_assignment" "assTest01Auditors" {
+  provider             = "azurerm.Project01"
+  scope                = azurerm_resource_group.rgTestRBACAssignment.id
+  role_definition_name = "Reader"
+  principal_id         = azuread_group.gProject01Auditors.id
 }
 
 #TODO: Add Policy Definition/Assignment
