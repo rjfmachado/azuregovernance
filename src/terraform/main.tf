63a675dbd9d569c64bb8be89d248cf532b8414c6
Ricardo Machado <rjfmachado@users.noreply.github.com>
2019-11-15 06:31:21 +0000

    move to azuread 0.7.0

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -43,7 +43,7 @@ provider "azurerm" {
 }
 
 provider "azuread" {
-  version       = "~> 0.6.0"
+  version       = "~> 0.7.0"
   client_id     = var.client_id
   tenant_id     = var.tenant_id
   client_secret = var.client_secret
