1eaa7520a2b84c59ca9d804a02282127416965b7
Ricardo Machado <rjfmachado@users.noreply.github.com>
2019-12-12 15:41:49 +0000

    updates

--- src/ops/localdev.sh
+++ src/ops/localdev.sh
@@ -1,6 +1,8 @@
 # dot source this script $ . ./localdev.sh
 # TODO: verify the tenant/subscription
+rm .terraform/ -rf
+az account set --subscription "ricardomachado.net (DEV)"
 export TF_VAR_deploymentStage=dev
 export TF_IN_AUTOMATION=false
-export TF_CLI_ARGS_init="-input=false -backend=true -backend-config=dev.env"
+export TF_CLI_ARGS_init="-input=false -backend=true -backend-config=dev.stage"
 echo "in dev environment"
