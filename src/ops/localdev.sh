0b69c7eda5efd58d0f79a7cb74bedbe126e179dd
Ricardo Machado <rjfmachado@users.noreply.github.com>
2019-12-12 10:43:44 +0000

    refactor workflow

--- src/ops/localdev.sh
+++ src/ops/localdev.sh
@@ -1,4 +1,5 @@
-# dot source this script $ . ../ops/localdev.sh
+# dot source this script $ . ./localdev.sh
+# TODO: verify the tenant/subscription
 export TF_VAR_deploymentStage=dev
 export TF_IN_AUTOMATION=false
 export TF_CLI_ARGS_init="-input=false -backend=true -backend-config=dev.env"
--- src/ops/localprod.sh
+++ src/ops/localprod.sh
@@ -1,4 +1,5 @@
-# dot source this script $ . ../ops/localprod.sh
+# dot source this script $ . ./localprod.sh
+# TODO: verify the tenant/subscription
 export TF_VAR_deploymentStage=prod
 export TF_IN_AUTOMATION=false
 export TF_CLI_ARGS_init="-input=false -backend=true -backend-config=prod.env"
