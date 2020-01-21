943768bf2a5788d0e9c4d0c1b8033a742c995e7f
Ricardo Machado <rjfmachado@users.noreply.github.com>
2020-01-21 10:03:22 +0000

    updates to env scripts

--- src/ops/localdev.sh
+++ src/ops/localdev.sh
@@ -1,8 +1,15 @@
 # dot source this script $ . ./localdev.sh
-# TODO: verify the tenant/subscription
+
 rm .terraform/ -rf
-az account set --subscription "ricardomachado.net (DEV)"
+
 export TF_VAR_deploymentStage=dev
 export TF_IN_AUTOMATION=false
 export TF_CLI_ARGS_init="-input=false -backend=true -backend-config=dev.stage"
-echo "in dev environment"
+
+if az account set --subscription "ricardomachado.net (DEV)";
+then
+    echo "in dev environment, please ensure the correct branch is selected:"
+    git branch
+else
+    echo "failed to select the correct subscription, please run az login."
+fi
--- src/ops/localprod.sh
+++ src/ops/localprod.sh
@@ -1,8 +1,15 @@
 # dot source this script $ . ./localprod.sh
-# TODO: verify the tenant/subscription
+
 rm .terraform/ -rf
-az account set --subscription "ricardomachado.net"
+
 export TF_VAR_deploymentStage=prod
 export TF_IN_AUTOMATION=false
 export TF_CLI_ARGS_init="-input=false -backend=true -backend-config=prod.stage"
-echo "in prod environment"
+
+if az account set --subscription "ricardomachado.net";
+then
+    echo "in prod environment, please ensure the correct branch is selected:"
+    git branch
+else
+    echo "failed to select the correct subscription, please run az login."
+fi
