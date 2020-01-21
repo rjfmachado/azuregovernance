23638636bacf8b0da84a78825126311131a9562d
Ricardo Machado <rjfmachado@users.noreply.github.com>
2020-01-21 18:14:42 +0000

    Assign project01 subscriptions

--- src/terraform/mgProject01.tf
+++ src/terraform/mgProject01.tf
@@ -8,5 +8,5 @@ resource "azurerm_management_group" "mgManagedProject01" {
   display_name               = "My Managed Application Project 01"
   parent_management_group_id = azurerm_management_group.mgManaged.id
   group_id                   = "mgProject01"
-  #subscription_ids           = var.mgManagedProject01Subscriptions[var.deploymentStage]
+  subscription_ids           = var.mgManagedProject01Subscriptions[var.deploymentStage]
 }
