7c362d7599f685284896e548ba463a30e9ebd474
Ricardo Machado <rjfmachado@users.noreply.github.com>
2020-06-27 07:28:45 +0000

    update subscription assignments

--- src/terraform/mgNetworking.tf
+++ src/terraform/mgNetworking.tf
@@ -1,12 +1,12 @@
-variable "mgNetworkingSubscriptions" {
-  type        = map(list(string))
-  description = "The subscriptions linked to Networking Management Group"
-  default     = null
-}
+# variable "mgNetworkingSubscriptions" {
+#   type        = map(list(string))
+#   description = "The subscriptions linked to Networking Management Group"
+#   default     = null
+# }
 
-resource "azurerm_management_group" "mgNetworking" {
-  display_name               = "Networking"
-  parent_management_group_id = data.azurerm_management_group.mgTenantRoot.id
-  name                       = "Networking"
-  #subscription_ids           = var.mgNetworkingSubscriptions[var.deploymentStage]
-}
+# resource "azurerm_management_group" "mgNetworking" {
+#   display_name               = "Networking"
+#   parent_management_group_id = data.azurerm_management_group.mgTenantRoot.id
+#   name                       = "Networking"
+#   subscription_ids           = var.mgNetworkingSubscriptions[var.deploymentStage]
+# }
--- src/terraform/mgSandboxProjects.tf
+++ src/terraform/mgSandboxProjects.tf
@@ -1,12 +1,19 @@
+variable "mgSandboxSubscriptions" {
+  type        = map(list(string))
+  description = "The subscriptions linked to the Sandbox Management Group"
+  default     = null
+}
+
 resource "azurerm_management_group" "mgSandbox" {
   display_name               = "Sandbox Projects"
   parent_management_group_id = data.azurerm_management_group.mgTenantRoot.id
   name                       = "Sandbox"
+  subscription_ids           = var.mgSandboxSubscriptions[var.deploymentStage]
 
-  lifecycle {
-    ignore_changes = [
-      # Ignore changes to subscription assignments.
-      subscription_ids,
-    ]
-  }
+  # lifecycle {
+  #   ignore_changes = [
+  #     # Ignore changes to subscription assignments.
+  #     subscription_ids,
+  #   ]
+  # }
 }
